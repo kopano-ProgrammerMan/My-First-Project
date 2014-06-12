29f80316ef63e92ed3aa68cfdb5330f92edd8c73
kopano <kmaripane@it2.co.za>
2014-06-12 17:05:23 +0200

    Auth

--- application/forms/Login.php
+++ application/forms/Login.php
@@ -5,7 +5,30 @@ class Application_Form_Login extends Zend_Form
 
     public function init()
     {
-        /* Form Elements & Other Definitions Here ... */
+        $this->setname('Login');
+		$this->setMethod('post');
+		
+		$username = new Zend_Form_Element_Text('username');
+		$username	->setLabel('Username:')
+			->addFilter('StringTrim')
+			->addFilter('StringToLower')
+			->addvalidator('Stringlength', array('max' => 8, 'min' =>6 ))
+			->setRequired('true');
+					 
+					 
+		$password = new Zend_Form_Element_Text('password');
+		$password	->setLabel('Password:')
+			->addFilter('StringTrim')		
+			->addvalidator('Stringlength', array('max' => 8, 'min' =>6 ))
+			->setRequired('true');
+					 		
+		$submit	= new Zend_Form_Element_Submit('submit');
+		$submit	->setLabel('Login');
+	
+							
+				 
+ $this->addElements(array($username,$password,$submit));
+		
     }
 
 
